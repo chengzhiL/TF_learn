c0b6e4ff2600fca649fa514ff4939c0a1a0b90f7
liuchengzhi <chengzhiliu@126.com>
2024-05-01 21:46:49 +0800

    commit class2 and class3

--- tf/class1/p21_random.normal.py
+++ tf/class1/p21_random.normal.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 
 d = tf.random.normal([2, 2], mean=0.5, stddev=1)
 print("d:", d)
+# 方差为1，均值为0.5 截断
 e = tf.random.truncated_normal([2, 2], mean=0.5, stddev=1)
 print("e:", e)
--- tf/class1/p23_cast_reduce_minmax.py.py
+++ tf/class1/p23_cast_reduce_minmax.py.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 
 x1 = tf.constant([1., 2., 3.], dtype=tf.float64)
 print("x1:", x1)
+# cast 执行张量数据类型的转换
 x2 = tf.cast(x1, tf.int32)
 print("x2", x2)
+# 计算张量的最小值
 print("minimum of x2：", tf.reduce_min(x2))
 print("maxmum of x2:", tf.reduce_max(x2))
