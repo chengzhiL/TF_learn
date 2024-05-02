b2bfdf0cafc7daab302c234fb673fa43f210f175
liuchengzhi <chengzhiliu@126.com>
2024-05-02 15:20:20 +0800

    commit sequential-注释

--- tf/class3/p8_iris_sequential.py
+++ tf/class3/p8_iris_sequential.py
@@ -15,14 +15,17 @@ tf.random.set_seed(116)
 # 隐藏层：该模型包含一个具有3个输出单元的全连接层
 # 激活函数：全连接层使用softmax激活函数，用于多分类问题
 # 正则化：使用L2正则化约束模型的参数，防止过拟合
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(3, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2())
 ])
 
-model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.1),
+# y_ ---y_train 给的是数值。
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.2),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=['sparse_categorical_accuracy'])
 
 model.fit(x_train, y_train, batch_size=32, epochs=500, validation_split=0.2, validation_freq=20)
 
 model.summary()
+
